2f24a5e234ce8d63ba5af44181c4a4819e511ec9
Vladimir Kudashov <V.Kudashov.83@yandex.ru>
2019-01-14 20:03:19 +0300

    Update ui.

--- app/src/main/java/ru/qdev/kudashov/jokes/main/ui/MainFragment.kt
+++ app/src/main/java/ru/qdev/kudashov/jokes/main/ui/MainFragment.kt
@@ -6,14 +6,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
 import androidx.databinding.DataBindingUtil
 import com.trello.rxlifecycle3.android.lifecycle.kotlin.bindToLifecycle
 import com.trello.rxlifecycle3.components.support.RxFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.main_fragment.*
 import ru.qdev.kudashov.jokes.R
 import ru.qdev.kudashov.jokes.databinding.MainFragmentBinding
+import ru.qdev.kudashov.jokes.ui.ScrollViewExtended
 
 class MainFragment : RxFragment() {
 
@@ -43,6 +46,23 @@ class MainFragment : RxFragment() {
             }
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        scrollView.setOnScrollChangeSupportedListener(object : ScrollViewExtended.OnScrollChangeSupportListener {
+            override fun onScrollChange(v: View, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int) {
+                val scrollV = v as ScrollView
+                if (oldScrollY > scrollV.getChildAt(0).height - scrollV.height || oldScrollY < 0) return
+
+                val deltaY = scrollY - oldScrollY
+                if (deltaY > 0 && floatingActionButton.visibility == View.VISIBLE) {
+                    floatingActionButton.hide()
+                } else if (deltaY < 0 && floatingActionButton.visibility != View.VISIBLE) {
+                    floatingActionButton.show()
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
