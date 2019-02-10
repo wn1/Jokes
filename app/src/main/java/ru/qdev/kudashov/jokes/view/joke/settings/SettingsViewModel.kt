380252091ae62cf4faa8928ef09f374d4097c9ca
Vladimir Kudashov <v.kudashov.83@yandex.ru>
2019-02-10 15:19:01 +0300

    Joke list view added

--- app/src/main/java/ru/qdev/kudashov/jokes/view/joke/settings/SettingsViewModel.kt
+++ app/src/main/java/ru/qdev/kudashov/jokes/view/joke/settings/SettingsViewModel.kt
@@ -7,9 +7,15 @@ import ru.qdev.kudashov.jokes.model.repository.SettingsRepository
 
 class SettingsViewModel (application: Application) : AndroidViewModel(application) {
     private val settingsRepository = SettingsRepository(application)
-    val settingsPreference = MutableLiveData(settingsRepository.getSettingsPreference())
+    private val settingsPreference = settingsRepository.getSettingsPreference()
+
+    val notificationEnable
+        get() = settingsPreference.notificationEnable
+
+    val notificationCount
+        get() = settingsPreference.notificationCount.toString()
 
     fun saveSettings() {
-        settingsRepository.saveSettingsPreference(settingsPreference.value!!)
+        settingsRepository.saveSettingsPreference(settingsPreference)
     }
 }
